9b1e9f20a35b232c254a51fd6d92c4a1df98f93c
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2023-11-01 18:59:13 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header></header>
+
+  <main></main>
+</template>
---
+++ src/components/app-tabs/app-tabs.vue
@@ -0,0 +1,5 @@
+<template lang="html" src="./app-tabs.html"></template>
+
+<script lang="ts" src="./app-tabs.ts"></script>
+
+<!-- <style lang="scss"></style> -->
